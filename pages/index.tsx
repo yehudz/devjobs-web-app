63f48d14184caeb8494a314ba2d73a313ac5bf44
Yehuda Zeytim <yehuda.zeytim@gmail.com>
2022-08-22 23:42:57 +0300

    [DWA-4] Create Job Card UI (#2)
    Code clean up: Remove dummy data to create card
    Component should be available for server data

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,16 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = (props) => {
   return(
-    <>
-    </>
+    <div 
+      className="
+        main
+        h-screen
+        w-full
+        bg-light-grey
+        dark:bg-midnight 
+      "
+    >
+    </div>
   )
 }
 
