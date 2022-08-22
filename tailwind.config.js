9b427183aa2e54e4782be8561c59f812bbaf2976
yehudz <yehuda.zeytim@gmail.com>
2022-08-21 19:28:50 -0600

    DWA-1: AC1. And default color
    Modify Tailwind config to have default colors in the design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'violet': '#9e7f66',
+      'light-violet': '#939BF4',
+      'very-dard-blue': '#19202D',
+      'midnight': '#121721',
+      'white': '#FFFFFF',
+      'light-grey': '#F4F6F8',
+      'gray': '#9DAEC2',
+      'dark-gray': '#6E8098',
+    },
   },
   plugins: [],
 }
