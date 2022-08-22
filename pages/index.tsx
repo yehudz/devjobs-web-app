5f3b8e9a18ee027ae0c42953dd77ab20dd98a632
yehudz <yehuda.zeytim@gmail.com>
2022-08-22 11:43:41 -0600

    DWA-1: Code clean up and ACs met
    Jira: https://yehudz.atlassian.net/browse/DWA-1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,8 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-import SearchInput from '../components/ui/SearchInput'
-import ToggleSwitch from '../components/ui/ToggleSwitch'
-import CheckboxComponent from '../components/ui/Checkbox'
 
 const Home: NextPage = (props) => {
   return(
     <>
-      <CheckboxComponent />
     </>
   )
 }
