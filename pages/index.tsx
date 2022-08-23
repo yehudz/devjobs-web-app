6823b27935227f31a234ecf091a8fdd19389f75f
yehudz <yehuda.zeytim@gmail.com>
2022-08-23 16:01:58 -0600

    refactor: Data should come from getStaticProps
    SSR enabled for this page

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Button from '../components/ui/Button'
 import searchContext from '../context/searchContext'
 import Link from 'next/link'
 const Home: NextPage = (props) => {
-  const [jobPosts, setJobPosts] = useState(data)
+  const [jobPosts, setJobPosts] = useState(props.jobs)
   const [limit, setLimit] = useState<number>(12)
   const {
     searchQuery,
@@ -133,6 +133,24 @@ const Home: NextPage = (props) => {
       </div>
     </div>
   )
+}
+
+// This function gets called at build time on server-side.
+// It won't be called on client-side, so you can even do
+// direct database queries.
+export async function getStaticProps() {
+  // Call an external API endpoint to get posts.
+  // You can use any data fetching library
+  // import data from './data.json'
+  const jobs  = data
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      jobs,
+    },
+  }
 }
 
 export default Home
