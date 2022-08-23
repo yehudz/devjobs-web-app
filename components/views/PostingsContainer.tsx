28edd8327cea63b36ed107ed807a76370685c997
yehudz <yehuda.zeytim@gmail.com>
2022-08-22 18:49:18 -0600

    [DWA-6] Initial setup for component
    Create a postings container component

---
+++ components/views/PostingsContainer.tsx
@@ -0,0 +1,12 @@
+interface ContentProps {
+  children: JSX.Element
+}
+
+const PostingsContainer: React.FC<ContentProps> = ({children})=> {
+  return(
+    <>
+    </>
+  )
+}
+
+export default PostingsContainer
