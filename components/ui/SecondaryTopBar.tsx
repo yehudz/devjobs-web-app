b45e2edb9a06490fa80eaa0e99966179062e75ce
yehudz <yehuda.zeytim@gmail.com>
2022-08-22 18:14:47 -0600

    [DWA-13] Create secondary top bar container
    AC1. This should be able to hold the search bar as well as the page top banner.
    AC2. Should  be responsive.

---
+++ components/ui/SecondaryTopBar.tsx
@@ -0,0 +1,23 @@
+const SecondaryTopBar: React.FC = ()=> {
+  return(
+    <div
+      data-testid="secondary-top-bar-container"
+      className="
+        relative
+        h-[80px]
+        w-full
+        max-w-[90%]
+        xl:max-w-[1100px]
+        mx-auto
+        mt-[-40px]
+        bg-white
+        dark:bg-very-dark-blue
+        rounded-lg
+      "
+    >
+
+    </div>
+  )
+}
+
+export default SecondaryTopBar
